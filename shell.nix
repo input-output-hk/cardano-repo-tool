b6428e2b2d894cb5ec230873e4eb459d42c57be5
Kosyrev Serge <serge.kosyrev@iohk.io>
2020-01-15 16:29:30 +0300

    shell:  add niv

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
 let
   inherit (import ./. {}) pkgs nix-tools;
+  inherit (import ./lib.nix) niv;
 in
 nix-tools._raw.shellFor {
   packages    = ps: with ps; [ cardano-repo-tool ];
   buildInputs = (with nix-tools._raw; [
     cabal-install.components.exes.cabal
     ghcid.components.exes.ghcid
+    niv
   ]);
 }
