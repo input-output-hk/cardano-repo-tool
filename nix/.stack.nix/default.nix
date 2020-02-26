80c152991edbf3f67156a7507e8e7f7388a6b8bf
Craige McWhirter <craige.mcwhirter@iohk.io>
2020-02-26 12:03:24 +1000

    Added build dependency nix-archive

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -1,7 +1,10 @@
 {
   extras = hackage:
     {
-      packages = {} // { cardano-repo-tool = ./cardano-repo-tool.nix; };
+      packages = {} // {
+        cardano-repo-tool = ./cardano-repo-tool.nix;
+        nix-archive = ./nix-archive.nix;
+        };
       compiler.version = "8.6.5";
       compiler.nix-name = "ghc865";
       };
